466f5d1303d2cf7896653404367b722b40ce1dbd
Jonathan Howden <jonathanhowden@gmail.com>
2014-02-07 16:20:52 -0600

    messing around with more functions

--- mongodb_practice.rb
+++ mongodb_practice.rb
@@ -25,7 +25,7 @@ spacing
 
 spacing
 # Reading the first document in a collection, reading all of the documents
-# puts coll.find_one
+puts coll.find_one
 
 # coll.find.each { |row| puts row.inspect }
 
@@ -47,5 +47,68 @@ puts coll.count
 spacing
 # Getting a set of documents with a query
 
-puts coll.find("i" => {"$gt" => 90} ).to_a
-puts coll.find("i" => {"$gt" => 15, "$lte" => 19} ).to_a
+# puts coll.find("i" => {"$gt" => 90} ).to_a
+# puts coll.find("i" => {"$gt" => 15, "$lte" => 19} ).to_a
+
+spacing
+# Selecting a subset of fields for a query
+
+# data = coll.find({ "_id" => BSON::ObjectId('52f5465d2774e23258000001')}, :fields => ["name", "type"]).to_a
+# puts data.first["name"]
+# puts data.first["type"]
+# new_data = coll.find("name" => "Jonathan").to_a
+# puts new_data.first["name"]
+
+spacing
+# Querying with regex
+
+# puts coll.find({"name" => /^J/}).to_a
+
+params = {'search' => "tha"}
+search_string = params['search']
+puts coll.find({"name" => /#{search_string}/}).to_a
+
+spacing
+# Updating documents with update
+
+doc["name"] = "The Big Slamma Jamma"
+coll.update({"_id" => BSON::ObjectId('52f558772774e232ea000001')}, doc)
+puts coll.find({"_id" => BSON::ObjectId('52f558772774e232ea000001')}).to_a
+
+spacing
+# Deleting Documents with remove
+
+# coll.remove("i" => 71)
+# puts coll.count
+# puts coll.find("i" => 71).to_a
+
+# coll.remove
+puts coll.count
+
+spacing
+# Creating an index
+
+coll.create_index("i")
+
+# puts coll.find("_id" => BSON::ObjectId('52f558772774e232ea000001')).explain
+# puts
+# puts coll.find("i" => 49).explain
+# puts
+# puts coll.find("type" => "Human").explain
+
+puts coll.index_information
+
+spacing
+# Dropping an index, indexes, collections, and a database
+
+coll.drop_index("i_1")
+puts coll.index_information
+
+# coll.drop_indexes
+
+# coll.drop
+# puts db.collection_names.inspect
+
+# client.drop_database("mydb")
+client.database_names
+
